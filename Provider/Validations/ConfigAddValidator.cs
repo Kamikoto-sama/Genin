c8db1f0b54c17279788eefbd81a09c3a8cd431a8
Dan <38423206+Kamikoto-sama@users.noreply.github.com>
2023-02-23 18:58:25 +0500

    ConfigAddValidator fix

--- Provider/Validations/ConfigAddValidator.cs
+++ Provider/Validations/ConfigAddValidator.cs
@@ -14,7 +14,7 @@ public class ConfigAddValidator : AbstractValidator<ConfigAddDto[]>
         RuleForEach(configs => configs.Select(config => config.Key))
             .Must(key => key.IsSignificant()).WithMessage("Key must not be null, empty or whitespace")
             .Must(key => keyRegex.IsMatch(key)).WithMessage("Key may consist of digits (0-9) and alphabet (a-z, A-Z) separated by forward slashes (/)")
-            .Must(key => key.StartsWith("/") || key.EndsWith("/")).WithMessage("Key must not start or end with slashes");
+            .Must(key => !key.StartsWith("/") && !key.EndsWith("/")).WithMessage("Key must not start or end with slashes");
 
         RuleFor(config => config)
             .NotEmpty().WithMessage("Must be more than 0 configs")
